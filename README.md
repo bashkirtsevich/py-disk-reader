b5e3d0e99e33603a1290da94b08839e14373724f
D.A.Bashkirtsev <bashkirtsevich@gmail.com>
2019-11-03 00:55:04 +0500

    FAT32 implementation

--- README.md
+++ README.md
@@ -14,12 +14,48 @@ with open("images/floppy2.img", "rb") as f:
     img = FAT12Reader(FileReader(f))
 
     files = list(img.root_dir)
-    
+
+    for i, n in enumerate(files):
+        print(i, n.name)
+
     bar = files[1]
     baz = bar.read()
     print(bar.name, sha1(baz).hexdigest(), baz)
 
-    bar = files[6]
+    print("----")
+
+    bar = list(files[6])
     for i, n in enumerate(bar):
         print(i, n.name)
+
+    baz = bar[7].read()
+    print(bar[7].name, sha1(baz).hexdigest(), baz)
+
+```
+
+## FAT32
+
+```python
+from hashlib import sha1
+from reader import FileReader
+
+with open("images/fat32.img", "rb") as f:
+    img = FAT32Reader(FileReader(f))
+
+    files = list(img.root_dir)
+
+    for i, n in enumerate(files):
+        print(i, n.name)
+
+    bar = files[3]
+    baz = bar.read()
+    print(bar.name, sha1(baz).hexdigest(), baz)
+
+    bar = list(files[1])
+    for i, n in enumerate(bar):
+        print(i, n.name)
+
+    baz = bar[3].read()
+    print(bar[3].name, sha1(baz).hexdigest(), baz)
+
 ```
--- signatures.py
+++ signatures.py
@@ -22,13 +22,13 @@ FAT12_SIGN = (
     (0x27, 4, 'VolumeID', '<I'),
     (0x2B, 11, 'VolumeLabel', '11s'),
     (0x36, 8, 'FSType', '8s'),
-    (0x3E, 448, 'BootLoader', '448s'),
+    #(0x3E, 448, 'BootLoader', '448s'),
     (0x1FE, 2, 'BootSignature', '<H')  # 55 AA
 )
 
 FAT12_ENTRY_PERMS = (0x0B, 1, 'B')
 
-FAT_12_ENTRY = (
+FAT12_ENTRY = (
     (0x00, 8, 'Name', '8s'),
     (0x08, 3, 'Ext', '3s'),
     (0x0B, 1, 'DOSPerms', 'B'),
@@ -43,7 +43,7 @@ FAT_12_ENTRY = (
     (0x1A, 2, 'ClusterLo', '<H'),
     (0x1C, 4, 'FileSize', '<I')
 )
-FAT_12_ENTRY_SIZE = 32
+FAT12_ENTRY_SIZE = 32
 
 FAT12_LFN = (
     (0x00, 1, 'SeqNumber', 'B'),  # LFN slot #
@@ -60,35 +60,41 @@ FAT16_SIGN = FAT12_SIGN
 
 FAT16_STRUCT = namedtuple("FAT16", [it[2] for it in FAT16_SIGN])
 
-FAT_32_SIGN = (
-    (0x00, 'chJumpInstruction', '3s'),
-    (0x03, 'chOemID', '8s'),
-    (0x0B, 'wBytesPerSector', '<H'),
-    (0x0D, 'uchSectorsPerCluster', 'B'),
-    (0x0E, 'wSectorsCount', '<H'),  # reserved sectors (min 32?)
-    (0x10, 'uchFATCopies', 'B'),
-    (0x11, 'wMaxRootEntries', '<H'),
-    (0x13, 'wTotalSectors', '<H'),
-    (0x15, 'uchMediaDescriptor', 'B'),
-    (0x16, 'wSectorsPerFAT', '<H'),  # not used, see 24h instead
-    (0x18, 'wSectorsPerTrack', '<H'),
-    (0x1A, 'wHeads', '<H'),
-    (0x1C, 'wHiddenSectors', '<H'),
-    (0x1E, 'wTotalHiddenSectors', '<H'),
-    (0x20, 'dwTotalLogicalSectors', '<I'),
-    (0x24, 'dwSectorsPerFAT', '<I'),
-    (0x28, 'wMirroringFlags', '<H'),  # bits 0-3: active FAT, it bit 7 set; else: mirroring as usual
-    (0x2A, 'wVersion', '<H'),
-    (0x2C, 'dwRootCluster', '<I'),  # usually 2
-    (0x30, 'wFSISector', '<H'),  # usually 1
-    (0x32, 'wBootCopySector', '<H'),  # 0x0000 or 0xFFFF if unused, usually 6
-    (0x34, 'chReserved', '12s'),
-    (0x40, 'chPhysDriveNumber', 'B'),
-    (0x41, 'chFlags', 'B'),
-    (0x42, 'chExtBootSignature', 'B'),
-    (0x43, 'dwVolumeID', '<I'),
-    (0x47, 'sVolumeLabel', '11s'),
-    (0x52, 'sFSType', '8s'),
+FAT32_SIGN = (
+    (0x00, 3, 'JumpInstruction', '3s'),
+    (0x03, 8, 'OemID', '8s'),
+    (0x0B, 2, 'BytesPerSector', '<H'),
+    (0x0D, 1, 'SectorsPerCluster', 'B'),
+    (0x0E, 2, 'SectorsCount', '<H'),  # reserved sectors (min 32?)
+    (0x10, 1, 'FATCopies', 'B'),
+    (0x11, 2, 'MaxRootEntries', '<H'),
+    (0x13, 2, 'TotalSectors', '<H'),
+    (0x15, 1, 'MediaDescriptor', 'B'),
+    #(0x16, 2, 'SectorsPerFAT', '<H'),  # not used, see 24h instead
+    (0x18, 2, 'SectorsPerTrack', '<H'),
+    (0x1A, 2, 'Heads', '<H'),
+    (0x1C, 2, 'HiddenSectors', '<H'),
+    (0x1E, 2, 'TotalHiddenSectors', '<H'),
+    (0x20, 4, 'TotalLogicalSectors', '<I'),
+    (0x24, 4, 'SectorsPerFAT', '<I'),
+    (0x28, 2, 'MirroringFlags', '<H'),  # bits 0-3: active FAT, it bit 7 set; else: mirroring as usual
+    (0x2A, 2, 'Version', '<H'),
+    (0x2C, 4, 'RootCluster', '<I'),  # usually 2
+    (0x30, 2, 'FSISector', '<H'),  # usually 1
+    (0x32, 2, 'BootCopySector', '<H'),  # 0x0000 or 0xFFFF if unused, usually 6
+    # (0x34, 12, 'Reserved', '12s'),
+    (0x40, 1, 'PhysDriveNumber', 'B'),
+    (0x41, 1, 'Flags', 'B'),
+    (0x42, 1, 'ExtBootSignature', 'B'),
+    (0x43, 4, 'VolumeID', '<I'),
+    (0x47, 11, 'VolumeLabel', '11s'),
+    (0x52, 8, 'FSType', '8s'),
     # ~ 0x72, 'chBootstrapCode', '390s'),
-    (0x1FE, 'wBootSignature', '<H')  # 55 AA
+    (0x1FE, 2, 'BootSignature', '<H')  # 55 AA
 )
+
+FAT32_LFN = FAT12_LFN
+
+FAT32_ENTRY = FAT12_ENTRY
+FAT32_ENTRY_SIZE = FAT12_ENTRY_SIZE
+FAT32_ENTRY_PERMS = FAT12_ENTRY_PERMS
